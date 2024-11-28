d315a583f054be24900677dc097b60b8241d9f67
Matthew Song <matthewsong18@gmail.com>
2024-11-27 22:28:47 -0700

    feat: add account check to pass test

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,17 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  user_name: String,
+  user_name: { type: String, unique: true, sparse: true },
   has_account: { type: Boolean, default: false },
   chats: [{ type: Schema.Types.ObjectId, ref: "Chat" }],
+});
+
+// Middleware: Validate user data before saving
+UserSchema.pre("save", function (next) {
+  if (!this.has_account && this.user_name) {
+    return next(new Error("Guest users cannot have a user_name."));
+  }
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);

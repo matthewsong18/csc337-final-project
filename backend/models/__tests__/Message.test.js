9ddaff41718192aabe1ee0651a77a888ed9227c6
Matthew Song <matthewsong18@gmail.com>
2024-11-28 00:56:23 -0700

    test: fix message data validation test

--- backend/models/__tests__/Message.test.js
+++ backend/models/__tests__/Message.test.js
@@ -35,13 +35,15 @@ describe("Message Schema", () => {
   });
 
   it("should ensure that a message has an author, chat, content, and date", async () => {
-    expect(async () => await Message.create({})).toThrow();
+    expect(async () => await Message.create({})).rejects.toThrow();
 
     const user = await User.create({ has_account: true, user_name: "Happy" });
-    expect(async () => await Message.create({ author: user })).toThrow();
+    expect(async () => await Message.create({ author: user })).rejects
+      .toThrow();
 
     const chat = await Chat.create({ users: [user] });
     expect(async () => await Message.create({ author: user, chat: chat }))
+      .rejects
       .toThrow();
   });
 });

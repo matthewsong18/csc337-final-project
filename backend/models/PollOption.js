ca58701b081af2a9d26737de03c0e6ba5efdec04
Matthew Song <matthewsong18@gmail.com>
2024-11-28 01:16:19 -0700

    feat: add default 0 to PollOption vote_count

--- backend/models/PollOption.js
+++ backend/models/PollOption.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const PollOptionSchema = new Schema({
   title: { type: String, required: true },
-  vote_count: Number,
+  vote_count: { type: Number, default: 0 },
 });
 
 const PollOption = mongoose.model("PollOption", PollOptionSchema);

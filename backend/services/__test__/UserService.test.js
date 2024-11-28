6dd10da90c4c89cea7cbf3d7c8009d208360e66e
Matthew Song <matthewsong18@gmail.com>
2024-11-27 23:42:20 -0700

    test: ensure accounts cannot have the same name

---
+++ backend/services/__test__/UserService.test.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const UserService = require("../UserService.js");
+const User = require("../../models/User.js");
+
+describe("UserService", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017");
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  beforeEach(async () => {
+    await User.deleteMany();
+  });
+
+  it("should prevent multiple accounts from having the same user_name", async () => {
+    const user1 = await UserService.createUser({
+      has_account: true,
+      user_name: "Bob",
+    });
+    const user2 = await UserService.createUser({
+      user_name: "Bob",
+      has_account: true,
+    });
+
+    expect(user1).toBeDefined();
+    expect(user1.user_name).toBe("Bob");
+    expect(user2).toBeUndefined();
+  });
+});

1ab4515f3ce10a80692db8970f5253a96c3e79be
Matthew Song <matthewsong18@gmail.com>
2024-11-28 00:57:25 -0700

    feat: add required to message data

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const MessageSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-  chat: { type: Schema.Types.ObjectId, ref: "Chat" },
-  content: String,
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  chat: { type: Schema.Types.ObjectId, ref: "Chat", required: true },
+  content: { type: String, required: true },
   date: { type: Date, default: Date.now() },
 });
 

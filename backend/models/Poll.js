1b6ace9738975e7b47eee886e35b3155342c705a
Matthew Song <matthewsong18@gmail.com>
2024-11-28 21:46:21 -0700

    feat: add Poll minimum one option requirement

--- backend/models/Poll.js
+++ backend/models/Poll.js
@@ -3,7 +3,15 @@ const { Schema } = mongoose;
 
 const PollSchema = new Schema({
   title: { type: String, required: true },
-  options: [{ type: Schema.Types.ObjectId, ref: "PollOption" }],
+  options: {
+    type: [{ type: Schema.Types.ObjectId, ref: "PollOption" }],
+    validate: {
+      validator: function (options) {
+        return options.length > 0;
+      },
+      message: "A poll must have at least one option.",
+    },
+  },
   users_voted: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 

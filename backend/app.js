a94f8e887c2a52583b4dd6f1741b476f8a4b5708
BaoNguyen09 <nttbvlogs@gmail.com>
2024-11-29 13:31:00 -0700

    fix: import path module, fix path to the frontend folder
    Mainly path module provides helpers to deal with paths in different environments, as Window vs Linux.
    So it may help if we deploy on cloud later

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 const chatRouter = require("./routes/chatRouter");
 const authRouter = require("./routes/authRouter");
 
 // Serve static files from the frontend folder globally
-app.use(express.static(path.join(__dirname, "../../frontend")));
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 
 app.use("/auth", authRouter);
 app.use("/chat", chatRouter);

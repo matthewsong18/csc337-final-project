63bf45f13b8489573f46dea35226c4d2b473d196
Claire Vu <v.claire47@gmail.com>
2024-11-29 20:18:26 -0700

    fix: separated app (use for testing) and server (actual server) into separate files

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 const indexRouter = require("./routes/indexRouter");
 const chatRouter = require("./routes/chatRouter");
 const authRouter = require("./routes/authRouter");
@@ -11,11 +12,6 @@ app.use("/", indexRouter);
 // Error page
 chatRouter.get("*", (req, res) => {
     res.send("* is a great way to catch all otherwise unmatched paths, e.g. for custom 404 error handling.");
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Achat app - listening on port ${PORT}!`);
 });
 
 module.exports = app;

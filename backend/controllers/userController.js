1726f47395bdd49354e7f478eac4ed6bc91d0f59
Claire Vu <v.claire47@gmail.com>
2024-11-29 21:57:30 -0700

    fix: added try and catch block to getUserbyName

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,8 @@ const UserService = require("../services/UserService");
 async function getUserByName(req, res) {
     const { username } = req.params;
 
-    const user = await db.getUserByName(username);
+    try {
+        const user = await UserService.findUser(username);
 
         // Call view for profile of user
         res.status(200).json(user);
@@ -16,57 +17,10 @@ async function getUserByName(req, res) {
 }
 
 async function createUser(req, res) {
-    const { username } = req.params;
-
-    const user = await db.getUserByName(username);
-
-    if (!user) {
-        // append to the db
-        await db.createUser(username);
-        // redirect to profile page
-        try {
-            res.redirect(`/profile/${username}`);
-        } catch (error) {
-            console.error(`Error: ${error}`);
-        }
-        console.log(`A new user is added: ${username}`);
-        return;
-    }
-
-    console.log("User is already in the db");
-    try {
-        res.redirect('/auth/login');
-    } catch (error) {
-
-        // Call view for error finding user
-        res.status(404).json({ message: error.message });
-    }
+        // Just placeholder
+        const { username } = req.params;
+        res.status(200).json(user);
 }
 
-// async function createUser(req, res) {
 
-//     if (!user) {
-//         // append to the db
-//         await db.createUser(username);
-//         // redirect to profile page
-//         try {
-//             res.redirect(`/profile/${username}`);
-//         } catch (error) {
-//             console.error(`Error: ${error}`);
-//         }
-//         console.log(`A new user is added: ${username}`);
-//         return;
-//     }
-
-//     console.log("User is already in the db");
-//     try {
-//         res.redirect('/auth/login');
-//     } catch (error) {
-//         console.error(`Error: ${error}`);
-//     }
-
-//     return;
-// };
-
-module.exports = { getUserByName, createUser };
 module.exports = { getUserByName, createUser};

c71bac770c5a35196020cbf537b594cd5ad566e4
Claire Vu <v.claire47@gmail.com>
2024-12-04 17:22:11 -0700

    fix: changed app.js and server.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
 const app = express();
 
 const indexRouter = require("./routes/indexRouter");
@@ -8,16 +7,6 @@ const chatRouter = require("./routes/chatRouter");
 const authRouter = require("./routes/authRouter");
 
 app.use(express.static(path.join(__dirname, "../frontend/public")));
-
-const mongoURI = "mongodb://localhost:27017/myDatabase";
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB on localhost");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
-  });
 
 app.use("/auth", authRouter);
 app.use("/chat", chatRouter);
@@ -28,7 +17,3 @@ chatRouter.get("*", (req, res) => {
 });
 
 module.exports = app;
-
-app.listen(3000, '127.0.0.1', () => {
-    console.log('Server is running at http://127.0.0.1:5000/');
-});

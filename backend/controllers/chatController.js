4e272832776fb2e682c6b0906a88f1bdf793a99f
BaoNguyen09 <nttbvlogs@gmail.com>
2024-12-07 16:21:13 -0700

    feat: implement function to load message buffer

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -2,6 +2,20 @@ const mongoose = require("mongoose");
 const path = require('path');
 const { Chat, Message, Poll  } = require("../models/index");
 
+async function load_message_buffer(message_ids, buffer_size, timestamp) {
+    let messages = [];
+    if (message_ids.length === 0) return messages;
+    // return x messages based on timestamps provided
+    messages = await Message.find({
+        _id: { $in: message_ids },
+        createdAt: { $lte: new Date(timestamp) }, // Filter messages before the given timestamp
+    })
+    .limit(buffer_size)
+    .populate("author", "user_name has_account")
+    .select("author content createdAt")
+    return messages;
+}
+
 function is_valid_timestamp(timestamp) {
     if (timestamp === null) {
         return false;
@@ -14,5 +28,6 @@ function is_valid_timestamp(timestamp) {
 }
 
 module.exports = {
+    load_message_buffer,
     is_valid_timestamp
 }

a72e9b002469288ecc420a536f98a7a33f350d6a
BaoNguyen09 <nttbvlogs@gmail.com>
2024-12-11 22:43:27 -0700

    fix: add validation back for ChatSchema

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -13,6 +13,12 @@ const ChatSchema = new Schema({
 	},
 	users: {
 		type: [{ type: Schema.Types.ObjectId, ref: "User" }],
+		validate: {
+			validator: function (users) {
+			  return users.length > 0;
+			},
+			message: "A chat must have at least one user.",
+		},
 	},
 	message: [{ type: Schema.Types.ObjectId, ref: "Message" }],
 	polls: [{ type: Schema.Types.ObjectId, ref: "Poll" }],

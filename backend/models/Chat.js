141a8005be6e73c2bc0cdc7ca1bb66f48c264596
Matthew William Song <matthewsong18@gmail.com>
2024-12-13 18:09:57 -0500

    20 add create message post (#69)

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ChatSchema = new Schema({
-	name: {
-		type: String,
-		required: false,
-	},
-	pin: {
-		type: Number,
-		required: false,
-		unique: true,
-	},
-	users: {
-		type: [{ type: Schema.Types.ObjectId, ref: "User" }],
-		validate: {
-			validator: function (users) {
-			  return users.length > 0;
-			},
-			message: "A chat must have at least one user.",
-		},
-	},
-	message: [{ type: Schema.Types.ObjectId, ref: "Message" }],
-	polls: [{ type: Schema.Types.ObjectId, ref: "Poll" }],
+  name: {
+    type: String,
+    required: false,
+  },
+  pin: {
+    type: Number,
+    required: false,
+    unique: true,
+  },
+  users: {
+    type: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    validate: {
+      validator: function (users) {
+        return users.length > 0;
+      },
+      message: "A chat must have at least one user.",
+    },
+  },
+  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  polls: [{ type: Schema.Types.ObjectId, ref: "Poll" }],
 });
 
 const Chat = mongoose.model("Chat", ChatSchema);
